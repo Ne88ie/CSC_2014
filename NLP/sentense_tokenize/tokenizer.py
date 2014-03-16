5995f9c91a3d87389f7b12529f535888aa240f85
Liana Bakradze <medeja-vector@yandex.ru>
2014-03-16 20:13:38 +0400

    good version of parser

--- NLP/sentense_tokenize/tokenizer.py
+++ NLP/sentense_tokenize/tokenizer.py
@@ -9,44 +9,49 @@ def tokenize(input_file, output_file):
     input = codecs.open(input_file, 'r', encoding="UTF8")
     sentence_text = []
     root = ET.Element('sentences')
+    lookup = None
     while True:
-        c = input.read(1)
+        if not lookup:
+            c = input.read(1)
+        else:
+            c = lookup
+            lookup = None
         if "" == c:
             break
-            #if c == '\n':
-        #    continue
-        sentence_text.append(c)
-        next = None
-        next_next = None
-        if c in ".?!":
-            flag = False
+        if c == '\n':
+            continue
+        if c == '"' or c == u'«':
+            sentence_text.append(c)
             next = input.read(1)
-            if next == '':
-                break
-            if next != " " and next != '\n':
+            while next != '"' and next != u'»':
                 sentence_text.append(next)
-            else:
                 next = input.read(1)
-                if next.isupper():
-                    flag = True
-                elif next == '"':
-                    next_next = input.read(1)
-                    if next_next.isupper():
-                        flag = True
-                    else:
+            sentence_text.append(next)
+            continue
+        sentence_text.append(c)
+        if c in ".!?":
+            next = None
+            if c == '.':
+                next = input.read(1)
+                if next != ' ' and next != '\n':
+                    sentence_text.append(next)
+                    continue
+                else:
+                    next_next = []
+                    while next == ' ' or next == '\n':
+                        next_next.append(next)
+                        next = input.read(1)
+                    if not next.isupper():
+                        sentence_text.extend(next_next)
                         sentence_text.append(next)
-                        sentence_text.append(next_next)
                         continue
-
-            if flag:
-                sentence = ET.SubElement(root, 'sentence')
-                sentence.text = ''.join(sentence_text)
-                sentence_text = []
-                sentence_text.append(next)
-                if (next_next != None):
-                    sentence_text.append(next_next)
+            sentence = ET.SubElement(root, 'sentence')
+            sentence.text = ''.join(sentence_text).strip()
+            sentence_text = []
+            if next:
+                lookup = next
     sentence = ET.SubElement(root, 'sentence')
-    sentence.text = ''.join(sentence_text)
+    sentence.text = ''.join(sentence_text).strip()
     tree = ET.ElementTree(root)
     f = open(output_file, 'w')
     tree.write(f, encoding="UTF-8", pretty_print=True, xml_declaration=True)
